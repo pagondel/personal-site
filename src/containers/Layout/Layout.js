666b0fb61fdee37f34e8a567b66c1bfb7ca54d76
Patricio Gonzalez <pagondel@gmail.com>
2020-03-26 23:35:59 -0300

    moves site Layout to component

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
+
+import styles from './Layout.module.scss';
+import SiteInfo from '../../data/site-info';
+import Nav from '../../components/Nav/Nav';
+
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className={styles.App}>
+        <Helmet>
+          <title>{SiteInfo.title}</title>
+          <meta name="keywords" content={SiteInfo.keywords.join()} />
+          <meta name="description" content={SiteInfo.description} />
+          <script src={SiteInfo.fontAwesome} cossorigin="anonymous" />
+        </Helmet>
+        <header className={styles.Header}>
+          <Nav
+            title={SiteInfo.title}
+          />
+        </header>
+        <aside className={styles.Sidebar}>sidebar</aside>
+        <main className={styles.Content}>content</main>
+        <footer className={styles.Footer}>footer</footer>
+      </div>
+    );
+  }
+}
+
+export default Layout;
