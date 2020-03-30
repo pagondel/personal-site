2fc96b260fdc5af355089bc311e4e6328e76ed11
Patricio Gonzalez <pagondel@gmail.com>
2020-03-29 23:18:28 -0300

    adds react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import Layout from './containers/Layout/Layout';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <Layout />
+    <Layout>
+      <Route exact path="/resume" render={() => <h1>Resume</h1>} />
+      <Route exact path="/" component={Home} />
+    </Layout>
   );
 }
 
