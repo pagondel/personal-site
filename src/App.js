49247310a6c7f86ea4dc44735eda68c736be3a71
Patricio Gonzalez <pagondel@gmail.com>
2020-03-31 16:27:21 -0300

    adds resume

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Route } from 'react-router-dom';
 
 import Layout from './containers/Layout/Layout';
 import Home from './components/Home/Home';
+import Resume from './components/Resume/Resume';
 
 function App() {
   return (
     <Layout>
-      <Route exact path="/resume" render={() => <h1>Resume</h1>} />
+      <Route exact path="/resume" component={Resume} />
       <Route exact path="/" component={Home} />
     </Layout>
   );
