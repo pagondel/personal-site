3ed72ad399e4dcda081d577738366cc92bbcde9b
Patricio Gonzalez <pagondel@gmail.com>
2020-04-08 00:16:05 -0400

    * Updates resume and home content. * Closes nav on click link on mobile * styles paragraph and links in home * removes font-size for resume info item

--- src/data/home.js
+++ src/data/home.js
@@ -5,14 +5,36 @@ const data = {
     <>
       <span>Hola!</span>
       {' '}
-      <span>Soy Pato,</span>
+      <span>Soy Pato.</span>
     </>
   ),
-  text: `desarrollor web con ${new Date().getFullYear() - 2012} años de experiencia en multiples lenguajes de `
-        + 'programación y plataformas. Actualmente'
-        + ' me desarrollo como software developer engineer en Groupon/Peixe, miembro del equipo encargado de '
-        + ' disponibilizar herramientas requeridas para el envío de correos masivos, logrando envíar millones correos '
-        + 'diarios.',
+  text: (
+    <>
+      <p>
+        Desarrollador con
+        {' '}
+        {new Date().getFullYear() - 2012}
+        {' '}
+        años de experiencia en multiples lenguajes de programación, durante este tiempo he
+        {' '}
+        trabajando tanto en grandes empresas como pequeñas startups.
+      </p>
+      <p>
+        Si bien soy un desarrollador full-stack capaz, mi fuerte se encuentra en el desarrollo de
+        {' '}
+        servicios back-end.
+      </p>
+      <p>
+        Actualmente me desarrollo como software developer engineer en
+        {' '}
+        <a href="https://www.peixe.cl" target="_blank" rel="noopener noreferrer">Groupon/Peixe</a>
+        {' '}
+        , miembro del equipo encargado de disponibilizar herramientas requeridas para el envío de
+        {' '}
+        correos masivos, logrando envíar millones correos diarios.
+      </p>
+    </>
+  ),
   photo: `${process.env.PUBLIC_URL}/photo.jpg`,
   photoAlt: 'Patricio Gonzalez photo',
 };
